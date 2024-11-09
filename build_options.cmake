7f41325ead85926204d9e55868e9d3de8b172ae4
grigory kalashnikov <gk>
2024-11-09 16:31:34 +0300

    cleanup

--- build_options.cmake
+++ build_options.cmake
@@ -16,36 +16,7 @@ macro (set_build_options PROJECTS_ROOT_DIR)
     include(version)
     gen_version_file(${PROJECTS_ROOT_DIR})
 
-    add_compile_definitions(V_COMPANY_NAME="Proxia")
-
-    add_compile_definitions(
-        COMMON_4=1
-        IOT_PLATFORM
-        SINGLE_STORER_THREAD=1
-        USE_IP_ADDRESSES
-        )
-
-    add_compile_definitions(
-        LOG_LEVEL_BY_PRIORITET=1
-        LOG_JUSTIFIED=1
-        LOG_FILE_NAME_WIDTH=20
-        LOG_CATEGORY_WIDTH=16
-        LOG_CATEGORY_ENABLED=0
-        )
-
-    add_compile_definitions(
-        LEFT_BOUND_TIME_4772=10800000
-        RIGHT_BOUND_TIME_4772=181440000
-        )
-
-    if (${TARGET_OS} STREQUAL "linux-arm")
-        add_compile_definitions(
-            ONLY_LINUX_ARM=1
-            UNC_L_BUILD=1
-            USE_ONE_DEVICE_CONFIG=1
-            LOADING_METER_DRIVERS_NOT_ALLOWED_ANYWAY=1
-            )
-    endif ()
+    add_compile_definitions(V_COMPANY_NAME="Company")
 
     if (MSVC)
       add_compile_definitions(_CRT_SECURE_NO_WARNINGS _CRT_SECURE_NO_DEPRECATE)
